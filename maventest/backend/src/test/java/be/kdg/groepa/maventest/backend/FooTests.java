5a072188251b361be074f632a59ec738f99c89b4
Wim Tibackx <wim.tibackx@student.kdg.be>
2014-02-01 16:28:21 +0100

    First iteration of maventest: backend + parent pom, and a (for now) fairly decent .gitignore
    Signed-off-by: Wim Tibackx <wim.tibackx@student.kdg.be>

---
+++ maventest/backend/src/test/java/be/kdg/groepa/maventest/backend/FooTests.java
@@ -0,0 +1,13 @@
+package be.kdg.groepa.maventest.backend;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FooTests {
+
+    @Test
+    public void fooTest() {
+        assertEquals(4,(2+2));
+    }
+}
